21d7f6f24daadbba404422fd3bc0b1ce4f67890d
Raphael Müßeler <raphael.muesseler@sap.com>
2025-03-11 11:34:12 +0100

    added basic transaction test

---
+++ test/cypress/e2e/transactions.spec.ts
@@ -0,0 +1,34 @@
+import { TestData } from "@/test/cypress/tasks/databaseTasks"
+
+describe("Transactions", () => {
+    let testData: TestData
+
+    beforeEach(() => {
+        cy.resetAndSeedDatabase(result => {
+            testData = result as TestData
+            cy.loginWithUser(testData.users.standard)
+            cy.visit("/dashboard")
+            cy.get("a[href='/transactions']").click()
+        })
+    })
+
+    it("should be able to create a transaction and delete it", () => {
+        cy.get("a[href='/transactions/new']").click()
+
+        cy.get("label[for='accountId'] + div").type("My Account{enter}")
+        cy.get("label[for='categoryId'] + div").type("Income{enter}")
+        cy.get("input[name='name']").type("Salary")
+        cy.get("label[for='type'] + div").type("Income{enter}")
+        cy.get("input[name='amount']").type("100.00")
+        cy.get("button[type='submit']").click()
+
+        cy.get(":nth-child(1) > .text-md").should("contain.text", "Salary")
+
+        cy.get(".bg-destructive").click()
+        cy.get(".bg-primary").click()
+        cy.url().should("satisfy", (str: string) => str.endsWith("/transactions"))
+
+        cy.get("tbody tr").should("have.length", 0)
+    })
+
+})
