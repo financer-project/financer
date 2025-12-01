f0a5934583cdc8bc553c5aa1e4cc4b9ff323c94e
Raphael Müßeler <raphael.muesseler@sap.com>
2025-12-01 16:17:28 +0100

    improved design of login and sign up

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -2,12 +2,14 @@ import { ResetPasswordForm } from "@/src/app/(auth)/components/ResetPasswordForm
 import { Button } from "@/src/lib/components/ui/button"
 import Link from "next/link"
 import { ArrowLeftIcon } from "lucide-react"
+import Logo from "@/src/lib/components/content/Logo"
 
 export default function ResetPasswordPage() {
     return (
         <main>
-            <div className={"flex flex-col w-lg h-screen justify-center items-start mx-auto gap-4"}>
-                <Button variant={"ghost"}
+            <div className={"flex flex-col max-w-lg h-screen justify-center mx-auto gap-4"}>
+                <Logo />
+                <Button variant={"secondary"}
                         asChild>
                     <Link href={"/"}>
                         <ArrowLeftIcon />
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,12 +2,14 @@ import { SignupForm } from "@/src/app/(auth)/components/SignupForm"
 import { ArrowLeftIcon } from "lucide-react"
 import Link from "next/link"
 import { Button } from "@/src/lib/components/ui/button"
+import Logo from "@/src/lib/components/content/Logo"
 
 export default function SignUpPage() {
     return (
         <main>
-            <div className={"flex flex-col w-lg h-screen justify-center items-start mx-auto gap-4"}>
-                <Button variant={"ghost"}
+            <div className={"flex flex-col max-w-lg h-screen justify-center mx-auto gap-4"}>
+                <Logo />
+                <Button variant={"secondary"}
                         asChild>
                     <Link href={"/"}>
                         <ArrowLeftIcon />
